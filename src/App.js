a976f96f7c806ca12b0e0c4858d0a2d0f021211b
joselineem <joselineem@gmail.com>
2024-04-25 18:18:38 -0700

    IS-AdvantagesSection

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.scss';
-import { MainBanner, PromosSection, WhyUsSection } from './pages/Home';
+import {
+  AdvantagesSection,
+  MainBanner,
+  PromosSection,
+  WhyUsSection,
+} from './pages/Home';
 
 function App() {
   return (
     <div className='App'>
       <MainBanner />
       <PromosSection />
-
+      <AdvantagesSection />
       <WhyUsSection />
     </div>
   );
