0a3d15c34889bcce80652df2846fe284dc3cb6ba
joselineem <joselineem@gmail.com>
2024-04-30 15:12:32 -0700

    footer creation

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,5 +4,14 @@ import MainBanner from './MainBanner/MainBanner';
 import WhyUsSection from './WhyUs/WhyUsSection';
 import Methodology from './Methodology/Methodology';
 import MissionVision from './MissionVision/MissionVision';
+import InfoFooter from './InfoFooter/InfoFooter';
 
-export { AdvantagesSection, PromosSection, MainBanner, WhyUsSection, Methodology, MissionVision};
+export {
+  AdvantagesSection,
+  PromosSection,
+  MainBanner,
+  WhyUsSection,
+  Methodology,
+  MissionVision,
+  InfoFooter,
+};
