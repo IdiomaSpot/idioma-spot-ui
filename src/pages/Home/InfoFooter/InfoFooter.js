4d06cc576ab79beb01decdbe48c413042c60793e
joselineem <joselineem@gmail.com>
2024-04-30 15:27:05 -0700

    IS-footer adding responsive behaviour

--- src/pages/Home/InfoFooter/InfoFooter.js
+++ src/pages/Home/InfoFooter/InfoFooter.js
@@ -11,14 +11,12 @@ const InfoFooter = () => {
             2225257987
           </a>
         </li>
-        |
         <li className='email'>
           <b>Email:</b>{' '}
           <a className='link-color' href='mailto:idiomaspot@gmail.com'>
             idiomaspot@gmail.com
           </a>
         </li>
-        |
         <li className='whatsapp'>
           <b>Whatsapp:</b>{' '}
           <a className='link-color' href='https://wa.me/2225257987'>
