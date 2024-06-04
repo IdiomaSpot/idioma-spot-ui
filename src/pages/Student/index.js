3e9f8c592adf99fa51131f78721ed45e09505b34
joselineem <joselineem@gmail.com>
2024-06-04 10:05:39 -0700

    fix fix

--- src/pages/Student/index.js
+++ src/pages/Student/index.js
@@ -2,7 +2,7 @@ import StudentDashboard from './StudentDashboard';
 import HomeContent from './HomeContent/HomeContent';
 import PaymentContent from './PaymentContent/PaymentContent';
 import Enrollment from './Enrollment/Enrollment';
-import ClassesContent from './ClassesContent copy/ClassesContent';
+import ClassesContent from './ClassesContent/ClassesContent';
 export {
   ClassesContent,
   Enrollment,
