b999652f976138356a6c1595a8355b5281002580
Antonio Chavez <antoniochavezdev@gmail.com>
2024-06-21 15:09:39 -0700

    Solving test issues

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,25 @@
-import { render } from '@testing-library/react';
-import App from './App';
-import 'intersection-observer';
+import { render } from "@testing-library/react";
+import App from "./App";
+import "intersection-observer";
 
-test('renders learn react link', () => {
+jest.mock("@mercadopago/sdk-react", () => ({
+  initMercadoPago: jest.fn((publicKey) => {
+    console.log(publicKey);
+  }),
+  Wallet: jest.fn(
+    ({
+      onReady,
+      onError,
+      onSubmit,
+      customization,
+      initialization,
+      brand,
+      locale,
+    }) => <div>Mocked Wallet Component</div>
+  ),
+}));
+
+test("renders learn react link", () => {
   render(<App />);
   // const linkElement = screen.getByText(/learn react/i);
   // expect(linkElement).toBeInTheDocument();
