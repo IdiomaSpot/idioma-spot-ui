de2ed817b4e57794d6802dadc498759af479e08b
joselineem <joselineem@gmail.com>
2024-06-24 14:31:43 -0700

    adding home summary endpoint

--- src/pages/Student/HomeContent/HomeContent.js
+++ src/pages/Student/HomeContent/HomeContent.js
@@ -1,5 +1,5 @@
 import { AnimatedTextWord } from '../../../components/ui';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import IsPointsSummary from './IsPointsSummary/IsPointsSummary';
 import { useDispatch, useSelector } from 'react-redux';
 import MyClassesSummary from './MyClassesSummary/MyClassesSummary';
@@ -10,10 +10,15 @@ import { motion } from 'framer-motion';
 import { useEffectOnce } from '../../../hooks/useEffectOnce';
 import { changeContent } from '../../../context/features/student/studentSlice';
 import { getMenuOption } from '../../../data/studentsMenu';
+import useStudentRequest from '../../../hooks/useStudentRequest';
 
 const HomeContent = () => {
   const student = useSelector((state) => state.student);
   const dispatch = useDispatch();
+  const [{ data, isLoading, hasError }, setRequest] = useStudentRequest();
+  const [clSummary, setClSummary] = useState();
+  const user = useSelector((state) => state.user);
+
   const args = {
     justifyContent: 'center',
     justifyItems: 'center',
@@ -23,7 +28,25 @@ const HomeContent = () => {
 
   useEffectOnce(() => {
     dispatch(changeContent(getMenuOption('home')));
+    if (user?.id) {
+      setRequest({
+        type: 'enrolled-classes',
+        data: {
+          userId: user?.id,
+        },
+      });
+    }
   });
+
+  useEffect(() => {
+    if (!isLoading && !hasError && data) {
+      if (data.length > 2) {
+        setClSummary(data.slice(0, 2));
+      } else {
+        setClSummary(data);
+      }
+    }
+  }, [isLoading, hasError, data]);
 
   return (
     <div className='home-content'>
@@ -36,7 +59,7 @@ const HomeContent = () => {
       </Box>
       <Grid container spacing={3}>
         <Grid item xs={12} sm={6} {...args}>
-          <MyClassesSummary />
+          <MyClassesSummary classes={clSummary} />
         </Grid>
         <Grid className='column-display' item xs={12} sm={6} {...args}>
           <IsPointsSummary points={student.isPoints} />
