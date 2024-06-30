86a402096f959dccb8c0ed71cfefa3c6d5c927a6
Antonio Chavez <antoniochavezdev@gmail.com>
2024-06-29 19:19:32 -0700

    86b12k7gc [FE] Adding missing fields within Admin Payment Page

--- src/pages/Admin/Payments/PaymentTable/PaymentTable.js
+++ src/pages/Admin/Payments/PaymentTable/PaymentTable.js
@@ -1,17 +1,53 @@
 import { DataGrid } from '@mui/x-data-grid';
+import { getStatus } from '../../../../utils/paymentUtils';
+import './PaymentTable.scss';
 
 const columns = [
   { field: 'paymentId', headerName: 'ID', width: 180 },
   { field: 'externalReference', headerName: 'Referencia de pago', width: 280 },
   { field: 'description', headerName: 'Clase', width: 110 },
+  { field: 'title', headerName: 'Nivel-Horario', width: 110 },
   {
     field: 'unitPrice',
     headerName: 'Pago',
     type: 'number',
     width: 60,
   },
-  { field: 'status', headerName: 'Estado', width: 130 },
-  { field: 'studentId', headerName: 'Estudiante', type: 'number', width: 90 },
+  {
+    field: 'status',
+    headerName: 'Estado',
+    width: 130,
+    renderCell: (value) => {
+      const statusObj = value?.value ? getStatus(value.value) : { name: 'No disponible' };
+      return (
+        <span className="space dot">
+          {statusObj.name}
+          {statusObj?.icon}
+        </span>
+      );
+    },
+  },
+  {
+    field: 'createdAt',
+    headerName: 'Fecha de pago',
+    type: 'dateTime',
+    width: 150,
+    valueGetter: (value) => value && new Date(value),
+  },
+  {
+    field: 'fullName',
+    headerName: 'Pagado por',
+    type: 'number',
+    width: 110,
+    valueGetter: (value, row) =>
+      `${row.user.name || ''} ${row.user.surname || ''}`,
+  },
+  {
+    field: 'user.email',
+    headerName: 'Email',
+    width: 110,
+    valueGetter: (value, row) => `${row.user.email || ''}`,
+  },
 ];
 
 export default function PaymentTable({ rows }) {
